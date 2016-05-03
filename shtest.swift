c1b8b31988e84d6067d2edc3fe088a069dba2293
Philipp Leo Dylong <creekpld@gmail.com>
2016-05-03 21:32:24 +0200

    first commit :)

---
+++ shtest.swift
@@ -0,0 +1,32 @@
+#!/bin/swift
+/**
+this file is for testing correct syntax highlighting of swift code.
+*/
+import Foundation
+import Glibc
+// this is a comment
+let players : Array = ["bob", "alice", "sam", "nobody"]
+let i : Int = 1
+var str :String = "i am a sting"
+let f: Float?
+let b:Bool = true
+var g:UInt64=123456
+// the \(str) variable inside the sting should not be highlighted
+print(players)
+print(players.count)
+print("print a String \(str).")
+/// aUInt8var is a variable name and should not be highlighted.
+func isNotZero(aUInt8var: UInt8) -> Bool{
+        if aUInt8var == 0 {
+                return false
+        }else{
+                return true
+        }
+}
+
+struct Video{
+	static var title: String
+	mutating func addStars (newVal: Int){
+
+	}
+}
